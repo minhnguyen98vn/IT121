415eb3872972af2d24c521120aa99ab6a16dd3e2
minhnguyen98vn <63829855+minhnguyen98vn@users.noreply.github.com>
2023-02-23 19:14:33 -0800

    Create hw1.js

---
+++ Web/js/hw1.js
@@ -0,0 +1,2 @@
+let firstName = "Minh", lastName = "Nguyen", yearOfStudy = "third year", goal = "to obtain flexible employment that supplements and supports my continuing education";
+const fullName = firstName + " " + lastName;
